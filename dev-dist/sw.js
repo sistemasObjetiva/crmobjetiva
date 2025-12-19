151a9057a390accebe1247ad3416151833e7f6eb
sistemasObjetiva <sistemas@objetiva.mx>
2025-12-19 14:00:26 -0600

    fix: update revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rgt4sr4vp1o"
+    "revision": "0.ckqsh66uuso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
