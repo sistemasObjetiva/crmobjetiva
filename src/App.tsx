7aadc0eef23c717f243bc4db301aca2858d8d8a6
sistemasObjetiva <sistemas@objetiva.mx>
2026-05-18 23:02:35 -0600

    Add routes for redirecting to login page

--- src/App.tsx
+++ src/App.tsx
@@ -75,6 +75,7 @@ function App() {
                 })}
 
               </Route>
+<Route path="/" element={<Navigate to="/login" replace />} />
               <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
           </StatusChipProvider>
